be971bb36c050026069789bc22b902440df88863
RavenP <ravenp.dev@gmail.com>
2023-06-19 04:58:10 +0800

    Exercise - take user inputs

---
+++ exercises/inputs_exercies.py
@@ -0,0 +1,10 @@
+# input exercise
+
+# Create a program that takes in username, age, and city
+# print it out on the screen
+
+username: str = input("Enter your username: ")
+age: int = int(input("How old are you?: "))
+city: str = input("City name: ")
+
+print(f"{username} is {age} years old and he was from {city} city")
