e8e0bee84a5b8f227d9bb92b30a07ab847dd38df
RavenP <ravenp.dev@gmail.com>
2023-06-19 04:37:40 +0800

    variables and type annotations

---
+++ basics/part_02.py
@@ -0,0 +1,19 @@
+# string
+msg: str = "Hello world, this is a variable"
+print(msg)
+print(type(msg))
+
+# integers
+number: int = 90
+print(number)
+print(type(number))
+
+# floats
+number_with_decimal: float = 99.9
+print(number_with_decimal)
+print(type(number_with_decimal))
+
+# booleans
+is_verified: bool = False
+print(is_verified)
+print(type(is_verified))
